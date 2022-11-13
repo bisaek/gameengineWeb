92fd288faba15ab1e9ee6f931da6aa26ae7a5414
bisaek <huggerbertram@gmail.com>
2022-11-13 23:23:55 +0100

    add discord server

--- docs/src/config.ts
+++ docs/src/config.ts
@@ -32,7 +32,7 @@ export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
 
 export const GITHUB_EDIT_URL = `https://github.com/bisaek/huggerengine/tree/master/docs`;
 
-export const COMMUNITY_INVITE_URL = `https://astro.build/chat`;
+export const COMMUNITY_INVITE_URL = `https://discord.gg/jQ9rcnJZWX`;
 
 // See "Algolia" section of the README for more information.
 export const ALGOLIA = {
