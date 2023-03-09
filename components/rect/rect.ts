5c81781c3a926b25a01c8502b58aa9c54a8fc186
bisaek <huggerbertram@gmail.com>
2023-03-09 10:43:38 +0100

    rect color options

--- components/rect/rect.ts
+++ components/rect/rect.ts
@@ -1,11 +1,10 @@
 import { window, GameObject, component } from "huggerengine";
 
 export default class rect extends component {
-  public color = "#000000";
   // public window;
   update() {
     if (!this.window.ctx) return;
-    this.window.ctx.fillStyle = this.color;
+    this.window.ctx.fillStyle = this.options.color;
     this.window.ctx.fillRect(
       this.gameObject.x,
       this.gameObject.y,
